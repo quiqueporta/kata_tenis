2e0871e220a8d1229b685f61d13ce4e05c703b52
Quique Porta <quiqueporta@gmail.com>
2014-10-08 17:58:27 +0200

    Actualizados los tests

--- test_tenis.py
+++ test_tenis.py
@@ -5,200 +5,171 @@ from tenis import Jugador, Partida
 
 class TestTenis(unittest.TestCase):
 
+    def setUp(self):
+        self.jugador_1 = Jugador()
+        self.jugador_2 = Jugador()
+        self.partida = Partida(self.jugador_1, self.jugador_2)
+
     def test_jugador_empieza_con_0_puntos(self):
-        jugador_1 = Jugador()
-        self.assertEqual(jugador_1.indice_puntos, 0)
-        self.assertEqual(jugador_1.puntos, '0')
+        self.assertEqual(self.jugador_1.indice_puntos, 0)
+        self.assertEqual(self.jugador_1.puntos, '0')
 
     def test_jugador_gana_puntos(self):
-        jugador = Jugador()
-        self.assertTrue(hasattr(jugador, 'ganar_punto'))
+        self.assertTrue(hasattr(self.jugador_1, 'ganar_punto'))
 
-        self.assertEqual(jugador.ganar_punto(), '15')
-        self.assertEqual(jugador.puntos, '15')
+        self.assertEqual(self.jugador_1.ganar_punto(), '15')
+        self.assertEqual(self.jugador_1.puntos, '15')
 
-        self.assertEqual(jugador.ganar_punto(), '30')
-        self.assertEqual(jugador.puntos, '30')
+        self.assertEqual(self.jugador_1.ganar_punto(), '30')
+        self.assertEqual(self.jugador_1.puntos, '30')
 
-        self.assertEqual(jugador.ganar_punto(), '40')
-        self.assertEqual(jugador.puntos, '40')
+        self.assertEqual(self.jugador_1.ganar_punto(), '40')
+        self.assertEqual(self.jugador_1.puntos, '40')
 
-        self.assertEqual(jugador.ganar_punto(), 'Ventaja')
-        self.assertEqual(jugador.puntos, 'Ventaja')
+        self.assertEqual(self.jugador_1.ganar_punto(), 'Ventaja')
+        self.assertEqual(self.jugador_1.puntos, 'Ventaja')
 
-        self.assertEqual(jugador.ganar_punto(), 'Juego')
-        self.assertEqual(jugador.puntos, 'Juego')
+        self.assertEqual(self.jugador_1.ganar_punto(), 'Juego')
+        self.assertEqual(self.jugador_1.puntos, 'Juego')
 
     def test_ganar_partida_jugador_1_40_a_0(self):
-        jugador_1 = Jugador()
-        jugador_2 = Jugador()
-        partida = Partida(jugador_1, jugador_2)
 
-        partida.ganar_punto_jugador_1()
-        self.assertEqual(partida.jugador_1.puntos, '15')
+        self.partida.ganar_punto_jugador_1()
+        self.assertEqual(self.partida.jugador_1.puntos, '15')
 
-        partida.ganar_punto_jugador_1()
-        self.assertEqual(partida.jugador_1.puntos, '30')
+        self.partida.ganar_punto_jugador_1()
+        self.assertEqual(self.partida.jugador_1.puntos, '30')
 
-        partida.ganar_punto_jugador_1()
-        self.assertEqual(partida.jugador_1.puntos, '40')
+        self.partida.ganar_punto_jugador_1()
+        self.assertEqual(self.partida.jugador_1.puntos, '40')
 
-        partida.ganar_punto_jugador_1()
-        self.assertEqual(partida.ganador, jugador_1)
+        self.partida.ganar_punto_jugador_1()
+        self.assertEqual(self.partida.ganador, self.jugador_1)
 
     def test_ganar_partida_jugador_2_40_a_0(self):
-        jugador_1 = Jugador()
-        jugador_2 = Jugador()
-        partida = Partida(jugador_1, jugador_2)
 
-        partida.ganar_punto_jugador_2()
-        self.assertEqual(partida.jugador_2.puntos, '15')
+        self.partida.ganar_punto_jugador_2()
+        self.assertEqual(self.partida.jugador_2.puntos, '15')
 
-        partida.ganar_punto_jugador_2()
-        self.assertEqual(partida.jugador_2.puntos, '30')
+        self.partida.ganar_punto_jugador_2()
+        self.assertEqual(self.partida.jugador_2.puntos, '30')
 
-        partida.ganar_punto_jugador_2()
-        self.assertEqual(partida.jugador_2.puntos, '40')
+        self.partida.ganar_punto_jugador_2()
+        self.assertEqual(self.partida.jugador_2.puntos, '40')
 
-        partida.ganar_punto_jugador_2()
-        self.assertEqual(partida.ganador, jugador_2)
+        self.partida.ganar_punto_jugador_2()
+        self.assertEqual(self.partida.ganador, self.jugador_2)
 
     def test_ganar_por_diferencia_de_dos(self):
 
-        jugador_1 = Jugador()
-        jugador_2 = Jugador()
-        partida = Partida(jugador_1, jugador_2)
+        self.partida.ganar_punto_jugador_2()
+        self.partida.ganar_punto_jugador_2()
+        self.partida.ganar_punto_jugador_2()
 
-        partida.ganar_punto_jugador_2()
-        partida.ganar_punto_jugador_2()
-        partida.ganar_punto_jugador_2()
+        self.partida.ganar_punto_jugador_1()
+        self.partida.ganar_punto_jugador_1()
+        self.partida.ganar_punto_jugador_1()
 
-        partida.ganar_punto_jugador_1()
-        partida.ganar_punto_jugador_1()
-        partida.ganar_punto_jugador_1()
+        self.assertFalse(self.partida.ganar_punto_jugador_1())
 
-        self.assertFalse(partida.ganar_punto_jugador_1())
-
-        partida.ganar_punto_jugador_1()
-        self.assertEqual(partida.ganador, jugador_1)
+        self.partida.ganar_punto_jugador_1()
+        self.assertEqual(self.partida.ganador, self.jugador_1)
 
     def test_ir_con_ventaja_perder_e_ir_a_iguales(self):
 
-        jugador_1 = Jugador()
-        jugador_2 = Jugador()
-        partida = Partida(jugador_1, jugador_2)
+        self.partida.ganar_punto_jugador_2()
+        self.partida.ganar_punto_jugador_2()
+        self.partida.ganar_punto_jugador_2()
 
-        partida.ganar_punto_jugador_2()
-        partida.ganar_punto_jugador_2()
-        partida.ganar_punto_jugador_2()
+        self.partida.ganar_punto_jugador_1()
+        self.partida.ganar_punto_jugador_1()
+        self.partida.ganar_punto_jugador_1()
 
-        partida.ganar_punto_jugador_1()
-        partida.ganar_punto_jugador_1()
-        partida.ganar_punto_jugador_1()
+        self.partida.ganar_punto_jugador_1()
 
-        partida.ganar_punto_jugador_1()
+        self.partida.ganar_punto_jugador_2()
 
-        partida.ganar_punto_jugador_2()
+        self.assertEqual(self.jugador_1.puntos, '40')
+        self.assertEqual(self.jugador_2.puntos, '40')
 
-        self.assertEqual(jugador_1.puntos, '40')
-        self.assertEqual(jugador_2.puntos, '40')
+        self.partida.ganar_punto_jugador_2()
+        self.partida.ganar_punto_jugador_1()
 
-        partida.ganar_punto_jugador_2()
-        partida.ganar_punto_jugador_1()
-
-        self.assertEqual(jugador_1.puntos, '40')
-        self.assertEqual(jugador_2.puntos, '40')
+        self.assertEqual(self.jugador_1.puntos, '40')
+        self.assertEqual(self.jugador_2.puntos, '40')
 
     def test_ir_a_ventaja_y_ganar_el_juego(self):
-        jugador_1 = Jugador()
-        jugador_2 = Jugador()
-        partida = Partida(jugador_1, jugador_2)
 
-        partida.ganar_punto_jugador_2()
-        partida.ganar_punto_jugador_2()
-        partida.ganar_punto_jugador_2()
+        self.partida.ganar_punto_jugador_2()
+        self.partida.ganar_punto_jugador_2()
+        self.partida.ganar_punto_jugador_2()
 
-        partida.ganar_punto_jugador_1()
-        partida.ganar_punto_jugador_1()
-        partida.ganar_punto_jugador_1()
+        self.partida.ganar_punto_jugador_1()
+        self.partida.ganar_punto_jugador_1()
+        self.partida.ganar_punto_jugador_1()
 
-        partida.ganar_punto_jugador_1()
+        self.partida.ganar_punto_jugador_1()
 
-        partida.ganar_punto_jugador_1()
+        self.partida.ganar_punto_jugador_1()
 
-        self.assertEqual(partida.ganador, jugador_1)
+        self.assertEqual(self.partida.ganador, self.jugador_1)
 
     def test_obtener_puntuacion_jugador_1(self):
-        jugador_1 = Jugador()
-        jugador_2 = Jugador()
-        partida = Partida(jugador_1, jugador_2)
 
-        self.assertEqual(partida.puntuacion_jugador_1, '0')
-        partida.ganar_punto_jugador_1()
-        partida.ganar_punto_jugador_1()
-        self.assertEqual(partida.puntuacion_jugador_1, '30')
-        partida.ganar_punto_jugador_1()
-        self.assertEqual(partida.puntuacion_jugador_1, '40')
-        partida.ganar_punto_jugador_1()
-        self.assertEqual(partida.puntuacion_jugador_1, 'Ventaja')
+        self.assertEqual(self.partida.puntuacion_jugador_1, '0')
+        self.partida.ganar_punto_jugador_1()
+        self.partida.ganar_punto_jugador_1()
+        self.assertEqual(self.partida.puntuacion_jugador_1, '30')
+        self.partida.ganar_punto_jugador_1()
+        self.assertEqual(self.partida.puntuacion_jugador_1, '40')
+        self.partida.ganar_punto_jugador_1()
+        self.assertEqual(self.partida.puntuacion_jugador_1, 'Ventaja')
 
     def test_obtener_puntuacion_jugador_2(self):
-        jugador_1 = Jugador()
-        jugador_2 = Jugador()
-        partida = Partida(jugador_1, jugador_2)
 
-        partida.ganar_punto_jugador_2()
-        self.assertEqual(partida.puntuacion_jugador_2, '15')
-        partida.ganar_punto_jugador_2()
-        self.assertEqual(partida.puntuacion_jugador_2, '30')
-        partida.ganar_punto_jugador_2()
-        self.assertEqual(partida.puntuacion_jugador_2, '40')
-        partida.ganar_punto_jugador_2()
-        self.assertEqual(partida.puntuacion_jugador_2, 'Ventaja')
+        self.partida.ganar_punto_jugador_2()
+        self.assertEqual(self.partida.puntuacion_jugador_2, '15')
+        self.partida.ganar_punto_jugador_2()
+        self.assertEqual(self.partida.puntuacion_jugador_2, '30')
+        self.partida.ganar_punto_jugador_2()
+        self.assertEqual(self.partida.puntuacion_jugador_2, '40')
+        self.partida.ganar_punto_jugador_2()
+        self.assertEqual(self.partida.puntuacion_jugador_2, 'Ventaja')
 
     def test_no_puntua_mas_de_juego(self):
-        jugador_1 = Jugador()
-        jugador_2 = Jugador()
-        partida = Partida(jugador_1, jugador_2)
 
-        partida.ganar_punto_jugador_1()
-        partida.ganar_punto_jugador_1()
-        partida.ganar_punto_jugador_1()
-        partida.ganar_punto_jugador_1()
-        partida.ganar_punto_jugador_1()
-        self.assertEqual(partida.puntuacion_jugador_1, 'Juego')
+        self.partida.ganar_punto_jugador_1()
+        self.partida.ganar_punto_jugador_1()
+        self.partida.ganar_punto_jugador_1()
+        self.partida.ganar_punto_jugador_1()
+        self.partida.ganar_punto_jugador_1()
+        self.assertEqual(self.partida.puntuacion_jugador_1, 'Juego')
 
     def test_obtener_ganador_jugador_1(self):
-        jugador_1 = Jugador()
-        jugador_2 = Jugador()
-        partida = Partida(jugador_1, jugador_2)
 
-        self.assertEqual(partida.ganador, None)
+        self.assertEqual(self.partida.ganador, None)
 
-        partida.ganar_punto_jugador_1()
-        self.assertEqual(partida.ganador, None)
-        partida.ganar_punto_jugador_1()
-        self.assertEqual(partida.ganador, None)
-        partida.ganar_punto_jugador_1()
-        self.assertEqual(partida.ganador, None)
-        partida.ganar_punto_jugador_1()
-        self.assertEqual(partida.ganador, jugador_1)
+        self.partida.ganar_punto_jugador_1()
+        self.assertEqual(self.partida.ganador, None)
+        self.partida.ganar_punto_jugador_1()
+        self.assertEqual(self.partida.ganador, None)
+        self.partida.ganar_punto_jugador_1()
+        self.assertEqual(self.partida.ganador, None)
+        self.partida.ganar_punto_jugador_1()
+        self.assertEqual(self.partida.ganador, self.jugador_1)
 
     def test_obtener_ganador_jugador_2(self):
-        jugador_1 = Jugador()
-        jugador_2 = Jugador()
-        partida = Partida(jugador_1, jugador_2)
 
-        self.assertEqual(partida.ganador, None)
+        self.assertEqual(self.partida.ganador, None)
 
-        partida.ganar_punto_jugador_2()
-        self.assertEqual(partida.ganador, None)
-        partida.ganar_punto_jugador_2()
-        self.assertEqual(partida.ganador, None)
-        partida.ganar_punto_jugador_2()
-        self.assertEqual(partida.ganador, None)
-        partida.ganar_punto_jugador_2()
-        self.assertEqual(partida.ganador, jugador_2)
+        self.partida.ganar_punto_jugador_2()
+        self.assertEqual(self.partida.ganador, None)
+        self.partida.ganar_punto_jugador_2()
+        self.assertEqual(self.partida.ganador, None)
+        self.partida.ganar_punto_jugador_2()
+        self.assertEqual(self.partida.ganador, None)
+        self.partida.ganar_punto_jugador_2()
+        self.assertEqual(self.partida.ganador, self.jugador_2)
 
     def test_obtener_nombre_jugadores(self):
         jugador_1 = Jugador('Rafa')
